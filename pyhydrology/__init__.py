769d4f0edb6e8e732126f2fea3b02f6bf0f43542
SanjeevBashyal <sanjeev.bashyal01@gmail.com>
2025-10-21 11:57:01 +0545

    ERA5_module done and now start working on mHM

--- pyhydrology/__init__.py
+++ pyhydrology/__init__.py
@@ -2,10 +2,14 @@ from .ERA5downloader import ERA5Downloader  # noqa: F401
 from .netcdf import NetCDFProcessor  # noqa: F401
 from .dem import DEMSampler  # noqa: F401
 
+# ERA5-Land module
+from . import ERA5_Land  # noqa: F401
+
 __all__ = [
     "ERA5Downloader",
     "NetCDFProcessor",
     "DEMSampler",
+    "ERA5_Land",
 ]
 
 __version__ = "0.2.0"
